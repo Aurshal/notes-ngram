a9d4267e1a74f258fe1f34df602db98b735af324
Kushal <kushalsubedi2@gmail.com>
2022-06-11 17:09:45 +0545

    bug fixed

--- gen_csv.py
+++ gen_csv.py
@@ -3,13 +3,15 @@ import json
 from collections import defaultdict
 from ngram.ngram import generate_N_grams
 
-with open("data/dict2.json", "r") as f:
+with open("data/dict.json", "r") as f:
     merged_d = json.load(f)
     val = {}
     for v in merged_d.values():
         val.update(v)
+print(len(val))
+print(val)
 def gen_notes_tags_csv():
-    note_tags = defaultdict(list)
+    note_tags = defaultdict(set)
     with open("raw/3_govt_urls_state_only.csv", "r") as f:
         data = csv.DictReader(f)
         for row in data:
@@ -19,16 +21,16 @@ def gen_notes_tags_csv():
                 ngram = generate_N_grams(note.lower(), i)
                 for gram in ngram:
                     if gram in val:
-                        note_tags[original_note].append(gram)
+                        note_tags[original_note].add(gram)
 
     with open("data/note_tags.csv", "w", newline='') as f:
         writer = csv.writer(f, delimiter = ',',quoting=csv.QUOTE_MINIMAL)
         writer.writerow(['notes', 'tags'])
         for key, value in note_tags.items():
             value = ','.join(value)
-            writer.writerow([key, value])
-gen_notes_tags_csv()
+            writer.writerow([value, key])
 
+gen_notes_tags_csv()
  
 
 # def get_notes(text):
